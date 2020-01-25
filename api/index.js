437f0f23d07041ecfb8e644d755e1eef2977e36f
Jack Ryan <jack.dixon.ryan@gmail.com>
2020-01-25 08:34:46 -0700

    Fixed PORT issue for millionth in this life.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const schema = require("./schema/index");
 const typeDefs = schema();
 const queries = require("./queries");
 const cors = require("cors");
+const PORT = 9090 || process.env.PORT;
 
 const resolvers = {
   Query: {
@@ -17,6 +18,6 @@ app.use(cors());
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: "/", cors: true });
 
-app.listen(5000, () => {
-  console.log(`spun up at localhost:5000`);
+app.listen(PORT, () => {
+  console.log(`spun up at ${PORT}`);
 });
